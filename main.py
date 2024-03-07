967bc872e60673af943f152a2ee70def3b5f6ba5
Nikita <ronivl@yandex.ru>
2024-03-07 12:38:41 +0300

    init

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+#13505139889hdhdbvjdkbf
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
